ca900ca7db25f9e3920ce80be0f177a440457660
emackinnon1 <emackinnon100390@gmail.com>
2025-02-24 10:11:21 -0700

    Might not need entity

--- xarm-controller/custom_components/entity.py
+++ xarm-controller/custom_components/entity.py
@@ -1,9 +1,13 @@
 """Base entity for XArm Controller."""
 
+from collections.abc import Callable, Coroutine
+from typing import Any, Concatenate
+
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.helpers.entity import Entity
 from homeassistant.const import ATTR_SERIAL_NUMBER
+from homeassistant.exceptions import HomeAssistantError
 
 from .const import DOMAIN
 from .coordinator import XArmControllerUpdateCoordinator
@@ -14,7 +18,9 @@ class XArmControllerEntity(Entity, CoordinatorEntity[XArmControllerUpdateCoordin
 
     _attr_has_entity_name = True
 
-    def __init__(self,) -> None:
+    def __init__(
+        self,
+    ) -> None:
         """Initialize the XArm Controller entity."""
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, self.config_entry[ATTR_SERIAL_NUMBER])},
@@ -28,3 +34,35 @@ class XArmControllerEntity(Entity, CoordinatorEntity[XArmControllerUpdateCoordin
     # def device_info(self) -> DeviceInfo:
     #     """Return device information about this XArm device."""
     #     return self.coordinator.get_xarm_model()
+
+
+def exception_handler[
+    _EntityT: XArmControllerEntity, **_P
+](
+    func: Callable[Concatenate[_EntityT, _P], Coroutine[Any, Any, Any]],
+) -> Callable[
+    Concatenate[_EntityT, _P], Coroutine[Any, Any, None]
+]:
+    """Decorate XArmAPI client calls to handle exceptions.
+
+    A decorator that wraps the passed in function, catches XarmAPI errors.
+    """
+
+    async def handler(self: _EntityT, *args: _P.args, **kwargs: _P.kwargs) -> None:
+        try:
+            await func(self, *args, **kwargs)
+        except Exception as error:
+            raise HomeAssistantError(
+                translation_domain=DOMAIN,
+                translation_key="communication_error",
+                translation_placeholders={"error": str(error)},
+            ) from error
+
+        # except AirGradientError as error:
+        #     raise HomeAssistantError(
+        #         translation_domain=DOMAIN,
+        #         translation_key="unknown_error",
+        #         translation_placeholders={"error": str(error)},
+        #     ) from error
+
+    return handler
